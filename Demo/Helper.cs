6a2371b4280de767ea8faeee4e1d70b44efcf2af
Mena Erian <mena3koko2karas1@gmail.com>
2025-07-30 20:43:35 +0300

    Part 01 Generics - SWAP Example

---
+++ Demo/Helper.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo
+{
+    internal static class Helper
+    {
+
+        public static void Swap<T>(ref T x, ref T y)/* where T : struct */
+        {
+            Console.WriteLine("=== SWAP ===");
+            T temp = x;
+            x = y;
+            y = temp;
+        }
+
+        public static void Print<T>(string Name)
+        {
+            Console.WriteLine($"Hello {Name}");
+        }
+
+        /// public static void Swap(ref object x, ref object y)
+        /// {
+        ///     //int NumberX  = (int)x; // UnBoxing
+        ///     Console.WriteLine("=== SWAP ===");
+        ///     object temp = x;
+        ///     x = y;
+        ///     y = temp;
+        ///     // Boxing && UnBoxing
+        ///     // UnSafe Casting
+        ///     // any type Can to be Object
+        /// }
+
+        /// public static void Swap(ref int x, ref int y)
+        /// {
+        ///     Console.WriteLine("=== SWAP ===");
+        ///     int temp = x;
+        ///     x = y;
+        ///     y = temp;
+        /// }
+        /// public static void Swap(ref double   x, ref double y)
+        /// {
+        ///     Console.WriteLine("=== SWAP ===");
+        ///     double temp = x;
+        ///     x = y;
+        ///     y = temp;
+        /// }
+        /// public static void Swap(ref Point x, ref Point y)
+        /// {
+        ///     Console.WriteLine("=== SWAP ===");
+        ///     Point temp = x;
+        ///     x = y;
+        ///     y = temp;
+        /// }
+    }
+}
--- Demo/Program.cs
+++ Demo/Program.cs
@@ -4,7 +4,39 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Generic SWAP - Example 01
+            /// int A = 3, B = 5;
+            /// Console.WriteLine($"A = {A}");
+            /// Console.WriteLine($"B = {B}");
+            /// Helper.Swap/*<int>*/(ref A, ref B);
+            /// Console.WriteLine($"A = {A}");
+            /// Console.WriteLine($"B = {B}");
+            /// 
+            /// Point P01 = new Point(10, 20);
+            /// Point P02 = new Point(50, 60);
+            /// Console.WriteLine($"P01 = {P01}");
+            /// Console.WriteLine($"P02 = {P02}");
+            /// Helper.Swap<Point>(ref P01, ref P02);
+            /// Console.WriteLine($"P01 = {P01}");
+            /// Console.WriteLine($"P02 = {P02}");
+
+            /// object A = 3, B = 5; // Boxing
+            /// Console.WriteLine($"A = {A}");
+            /// Console.WriteLine($"B = {B}");
+            /// Helper.Swap(ref A, ref B);
+            /// Console.WriteLine($"A = {A}");
+            /// Console.WriteLine($"B = {B}");
+
+            /// Point P01 = new Point(10, 20);
+            /// Point P02 = new Point(50, 60);
+            /// Console.WriteLine($"P01 = {P01}");
+            /// Console.WriteLine($"P02 = {P02}");
+            /// Helper.Swap(ref P01, ref P02);
+            /// Console.WriteLine($"P01 = {P01}");
+            /// Console.WriteLine($"P02 = {P02}"); 
+            #endregion
+        
+            
         }
     }
 }
