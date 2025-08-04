445cf6f2695a37f41170b53d6513e082b9390d2c
Mena Erian <mena3koko2karas1@gmail.com>
2025-08-04 16:08:42 +0300

    Range<T> Generic Class

---
+++ Assignment/Program.cs
@@ -0,0 +1,18 @@
+﻿
+namespace Assignment
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Range
+            Range<int> range = new Range<int>(minimum: 5, maximum: 10);
+
+            //Console.WriteLine(range.IsInRange(9));
+            Console.WriteLine(range.Length());
+
+            #endregion
+        }
+
+    }
+}
---
+++ Assignment/Range.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Assignment
+{
+    internal class Range<T> where T : IComparable<T>, INumber<T>
+    {
+        public T Minimum { get; set; }
+        public T Maximum { get; set; }
+        public Range(T minimum  , T maximum)
+        {
+            Minimum = minimum; // 5
+            Maximum = maximum; // 10
+        }
+
+        public bool IsInRange(T value)//7
+        {
+            var min = value.CompareTo(Minimum);
+            if (min == -1) return false;
+            var max = value.CompareTo(Maximum);
+            if (max == 1) return false;
+            if ((min >= 0) && max <= 0) return true;
+            return false;
+        }
+
+        public T Length()
+        {
+            return Maximum - Minimum;
+        }
+    }
+}
